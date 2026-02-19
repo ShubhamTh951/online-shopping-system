26d706c4c0da6a8255df37fa769e35f50a7e85e8
Shubham Thakur <shubhamth951@gmail.com>
2026-02-19 12:50:55 +0530

    feat: update main module

--- src/com/project/onlineshop/app/MainApp.java
+++ src/com/project/onlineshop/app/MainApp.java
@@ -5,35 +5,63 @@ import com.project.onlineshop.payment.UPIPayment;
 import com.project.onlineshop.product.Product;
 import com.project.onlineshop.service.InventoryService;
 import com.project.onlineshop.service.OrderService;
+import com.project.onlineshop.user.Admin;
 import com.project.onlineshop.user.Customer;
 
 public class MainApp {
 
     public static void main(String[] args) {
+        InventoryService inventory = new InventoryService();
+
+        Admin admin = new Admin(
+                1,"Admin",
+                "admin@mail.com",
+                "1234",
+                "9999",
+                "India",
+                "SUPER");
+        
+        Product laptop = new Product(
+                101,"Laptop",
+                "Gaming Laptop",
+                60000,5,
+                "Electronics");
+        
+        admin.addProduct(inventory, laptop);
+        inventory.displayProducts();
 
         Customer customer = new Customer(
-                1, "Shubham", "shubham@gmail.com",
-                "1234", "9999999999", "India"
-        );
-
-        Product laptop = new Product(
-                101, "Laptop", "Gaming Laptop",
-                60000, 5, "Electronics"
-        );
-
-        InventoryService inventory = new InventoryService();
-        inventory.addProduct(laptop);
+                    2,
+                    "Shubham",
+                    "user@mail.com",
+                    "1111",
+                    "8888",
+                    "India",
+                    100000);
+        
+        customer.login("user@mail.com", "1111");
+        customer.updateProfile("999999999", "Delhi");
+        customer.changePassword("1111", "2222");
 
         Cart cart = new Cart();
         cart.addItem(laptop, 1);
         cart.displayCart();
 
+        UPIPayment payment = new UPIPayment("user@upi");
+
         OrderService orderService = new OrderService();
-        var order = orderService.placeOrder(customer, cart);
+        var order = orderService .placeOrder(customer, cart, payment);
 
         if (order != null) {
-            UPIPayment payment = new UPIPayment("shubham@upi");
-            payment.pay(order.getTotalAmount());
+            System.out.println("Order placed successfully!");
+        } 
+        else {
+            System.out.println("Order Failed!");
         }
+
+        customer.viewOrderHistory();
+        orderService.displayAllOrders();
+
+        customer.logout();
     }
 }
