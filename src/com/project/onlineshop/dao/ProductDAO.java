8039ce48600b557821b0e1da121badc39b92d94f
Shubham Thakur <shubhamth951@gmail.com>
2026-03-03 19:45:56 +0530

    feat: implement ProductDAO save, findById and updateStock methods

---
+++ src/com/project/onlineshop/dao/ProductDAO.java
@@ -0,0 +1,63 @@
+package com.project.onlineshop.dao;
+
+import com.project.onlineshop.product.Product;
+import com.project.onlineshop.util.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+   public void saveProduct(Product product) throws Exception {
+      String sql = "INSERT INTO products (name, description, price, stock, category) VALUES (?, ?, ?, ?, ?)";
+
+      try (Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, product.getName());
+            stmt.setString(2, product.getDescription());
+            stmt.setDouble(3, product.getPrice());
+            stmt.setInt(4, product.getStock());
+            stmt.setString(5, product.getCategory());
+            
+            stmt.executeUpdate();
+            System.out.println("Product saved successfully.");
+      } 
+   }
+
+   public Product findById(int id) throws Exception {
+      String sql = "SELECT * FROM products WHERE product_id = ?";
+
+      try (Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+         
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            
+            if (rs.next()) {
+                return new Product(
+                        rs.getInt("product_id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getDouble("price"),
+                        rs.getInt("stock"),
+                        rs.getString("category")
+                );
+            }
+      }
+      return null;
+   }
+
+   public void updateStock(int productId, int newStock) throws Exception {
+      String sql = "UPDATE products SET stock = ? WHERE product_id = ?";
+
+      try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, newStock);
+            stmt.setInt(2, productId);
+
+            stmt.executeUpdate();
+        }
+   }
+
+}
